24c82f106d527456ed04f6c529a658069a1409b0
muratcangencturk <muratcangencturk@windowslive.com>
2025-06-27 12:24:42 +0300

    Fix ad handler quotes and add storage fallback

---
+++ js/ad-handler.js
@@ -0,0 +1,40 @@
+(function () {
+  const adLinks = [
+    'https://otieu.com/4/9495321',
+    'https://otieu.com/4/9495319',
+    'https://otieu.com/4/9495318',
+    'https://otieu.com/4/9497116',
+  ];
+
+  const delayMinutes = [2, 3, 4];
+
+  function scheduleAd() {
+    try {
+      if (sessionStorage.getItem('adShown')) return;
+      sessionStorage.setItem('adShown', 'yes');
+    } catch {
+      // sessionStorage may be unavailable (incognito, old browser, etc.)
+      if (window.__adShownFallback) return;
+      window.__adShownFallback = true;
+    }
+
+    const adUrl = adLinks[Math.floor(Math.random() * adLinks.length)];
+    const wait =
+      delayMinutes[Math.floor(Math.random() * delayMinutes.length)] * 60000;
+
+    setTimeout(() => {
+      window.open(adUrl, '_blank', 'noopener,noreferrer');
+    }, wait);
+  }
+
+  function activateOnInteraction() {
+    document.addEventListener('scroll', scheduleAd, { once: true });
+    document.addEventListener('click', scheduleAd, { once: true });
+  }
+
+  if (document.readyState === 'complete') {
+    activateOnInteraction();
+  } else {
+    window.addEventListener('load', activateOnInteraction);
+  }
+})();
