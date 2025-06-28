f245139c4fbd28702c1127b30e7de253b57c7092
muratcangencturk <muratcangencturk@windowslive.com>
2025-06-29 01:29:17 +0300

    Update DOMPurify to 3.x

--- src/sanitize.js
+++ src/sanitize.js
@@ -1,4 +1,4 @@
-import DOMPurify from 'https://cdn.jsdelivr.net/npm/dompurify@2.4.4/dist/purify.es.js';
+import DOMPurify from 'https://cdn.jsdelivr.net/npm/dompurify@3.2.6/dist/purify.es.js';
 export const sanitizeHTML = (html) => DOMPurify.sanitize(html);
 export const setSanitizedHTML = (el, html) => {
   el.innerHTML = DOMPurify.sanitize(html);
